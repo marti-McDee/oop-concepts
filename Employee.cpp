4040abf9284e5d4d728d36a44bd8f51cd9167b21
David Martinez <anddagger@gmail.com>
2022-12-18 17:06:19 -0600

    constructors

--- Employee.cpp
+++
@@ -1,9 +0,0 @@
-#include "Employee.h"
-
-Employee::Employee(std::string name, std::string company, int age) : name(name), company(company), age(age) {}
-
-void Employee::IntroduceYourself() {
-	std::cout << "Name - " << name << std::endl;
-	std::cout << "Company - " << company << std::endl;
-	std::cout << "Age - " << age << std::endl;
-}
